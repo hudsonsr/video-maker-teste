22affd2875c44ea35639192f95bd19e2072f284c
hudsonsr <hudsonsr@gmail.com>
2019-04-13 19:09:48 -0300

    feat: start text robot

--- robots/text.js
+++ robots/text.js
@@ -10,7 +10,10 @@ async function robot(content){
   async function fetchContentFromWikipedia(content){
     const algorithmiaAutenticated =  algorithmia(algorithmiaApiKey)
     const wikipediaAlgorithm = algorithmiaAutenticated.algo('web/WikipediaParser/0.1.2')
-   const wikipediaResponde = await wikipediaAlgorithm.pipe(content.searchTerm)
+   const wikipediaResponde = await wikipediaAlgorithm.pipe({
+       "lang" : "pt",
+       "articleName" : content.searchTerm
+   })
    wikipediaContent = wikipediaResponde.get()
    content.sourceContenOriginal=wikipediaContent.content
   }

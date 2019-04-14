20701902673191ce7d2bfe69157a7b9ee0ebb3d0
hudsonsr <hudsonsr@gmail.com>
2019-04-13 23:36:35 -0300

    feat: add input robot and state robot

--- index.js
+++ index.js
@@ -1,28 +1,15 @@
-const readline = require('readline-sync')
+
 const robots = {
-	//userinput: require('./robots/user-input.js')
-	text: require('./robots/text.js')
+	input: require('./robots/input.js'),
+	text: require('./robots/text.js'),
+	state :require('./robots/state.js')
 }
 async function start() {
-	const content = {}
 	
-	content.searchTerm = askAndReturnSearhTerm()
-	content.prefix = askAndReturnPrefix()
-
-	//robots.userinput(content)
-	await robots.text(content)
-
-	function askAndReturnSearhTerm() {
-		return readline.question('Digite o termo a ser pesquisado no wikipedia: ')
-	}
-
-	function askAndReturnPrefix(){
-		const prefixes = ['Quem e','O que e', 'A historia de']
-		const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Escolha uma opcao: ')
-		const selectedPrefixText = prefixes[selectedPrefixIndex]
-
-		return selectedPrefixText 
-	}
-	console.log(content)
+	robots.input()
+	await robots.text()
+	
+	const content = robots.state.load()
+	console.dir(content,{depht: null})
 }
 start()
---
+++ robots/input.js
@@ -0,0 +1,26 @@
+const readline = require('readline-sync')
+const state = require('./state.js')
+
+function robot(){
+    const content = {
+        maximumSentences: 7
+    }
+
+    content.searchTerm = askAndReturnSearhTerm()
+    content.prefix = askAndReturnPrefix()
+    state.save(content)
+
+    function askAndReturnSearhTerm() {
+        return readline.question('Digite o termo a ser pesquisado no wikipedia: ')
+    }
+
+    function askAndReturnPrefix(){
+        const prefixes = ['Quem e','O que e', 'A historia de']
+        const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Escolha uma opcao: ')
+        const selectedPrefixText = prefixes[selectedPrefixIndex]
+
+        return selectedPrefixText 
+    }
+}
+
+module.exports = robot
